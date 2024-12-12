7712da797e7f31419736667ce5f043c7bd001b11
corra <corrado.marzagalli@gmail.com>
2024-12-12 18:20:52 +0100

    added loading message

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,6 +5,7 @@ function SearchBar({pokemon, setPokemon, setIsDuplicated}) {
     const [searchValue, setSearchValue] = useState('');
     const [searchValueCopy, setSearchValueCopy] = useState('');    
     const [isClicked, setIsClicked] = useState(false);                
+    const [isLoading, setIsLoading] = useState(false);
 
     return (
         <div id='searchBar'>
@@ -12,13 +13,19 @@ function SearchBar({pokemon, setPokemon, setIsDuplicated}) {
             <input placeholder='Enter a pokemon name' value={searchValue} onChange={(evt) => setSearchValue(evt.target.value)} type="text"/>
             <button onClick={async (evt) => {
                 setIsClicked(true);                
-                setSearchValueCopy(searchValue);                
+                setSearchValueCopy(searchValue);      
+                setIsLoading(true);          
                 const pokemonFound = await findPokemon(searchValue);
+                setIsLoading(false);
                 setPokemon(pokemonFound);
                 setIsDuplicated(false);
             }}>Search</button>
-            <p>Results for [{searchValueCopy}]</p>            
-            {isClicked && !pokemon && <p id='searchBarError'>No results</p>}
+            <p>Results for [{searchValueCopy}]</p>                                    
+            {isLoading && <p id='loadingField'>Loading...</p>}
+            {
+                !isLoading && isClicked && !pokemon && 
+                <p id='searchBarError'>No results</p>
+            }                            
         </div>
     )
 }
